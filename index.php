f2b92eec75e6da4d8542d5772ac32d58c54fdd41
Юрий <ddslimsi@gmail.com>
2017-12-14 20:56:51 +0200

    index

--- index.php
+++ index.php
@@ -51,9 +51,9 @@ if (in_array($user_message, $pic) || !empty($matchpic[0])) {
      $gif = file_get_contents('http://vk-send.tk/get-photo.php');
      //$name = $gif[0]['owner_id'].'_'.$gif[0]['vk_item_id'].'_'.$gif[0]['access_key'];
      $name = 'gif';
-     file_put_contents($name.'.gif',file_get_contents($gif[0]['preview_gif']));
+     file_put_contents($name.'.gif',file_get_contents($gif));
      $url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$user_id.'&text='.$name;
-     //$url = 'https://api.telegram.org/bot'.$token.'/sendDocument?chat_id='.$user_id.'&document='.$gif;
+     $url = 'https://api.telegram.org/bot'.$token.'/sendDocument?chat_id='.$user_id.'&document=https://groupvkbot.herokuapp.com/gif.gif';
 
 
 } else {
